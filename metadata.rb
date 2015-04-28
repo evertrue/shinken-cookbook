19bff8292a4a513295ecb62787552ce8033117a4
Jeff Byrnes <jeff@evertrue.com>
2015-04-28 11:26:26 -0400

    Set long_description == README.md contents
    Follows established practice

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'EverTrue, Inc.'
 maintainer_email 'eric.herot@evertrue.com'
 license          'Apache 2.0'
 description      'Installs/Configures shinken'
-long_description 'Installs/Configures shinken'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.3.0'
 
 depends 'ark'
