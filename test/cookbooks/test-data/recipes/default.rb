38f4676a8a6b8eaf635fe7ffa325801d9e0f9969
Jeff Byrnes <jeff@evertrue.com>
2015-04-29 15:33:16 -0400

    Remove defaults from test-data cookbook

--- test/cookbooks/test-data/recipes/default.rb
+++ test/cookbooks/test-data/recipes/default.rb
@@ -1,21 +1,15 @@
 directory '/etc/shinken/hosts' do
   owner     'shinken'
   group     'shinken'
-  mode      0755
-  action    :create
   recursive true
 end
 
 file '/etc/shinken/hosts/host-to-delete.cfg' do
-  action :create
   owner  'shinken'
   group  'shinken'
-  mode   0644
 end
 
 file '/etc/shinken/hostgroups/hostgroup-to-delete.cfg' do
-  action :create
   owner  'shinken'
   group  'shinken'
-  mode   0644
 end
