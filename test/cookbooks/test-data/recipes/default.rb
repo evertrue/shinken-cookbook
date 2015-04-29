6f09bb0cc98676b35bab23bfb2e5d78354fefd0c
Jeff Byrnes <jeff@evertrue.com>
2015-04-29 15:34:37 -0400

    Remove nonexistent user from test-data
    This cookbook needs to run before shinken:default to test
    the deletion of a node config properly, but the shinken user
    doesn’t yet exist, so the convergence fails.
    
    Relates to #3

--- test/cookbooks/test-data/recipes/default.rb
+++ test/cookbooks/test-data/recipes/default.rb
@@ -1,15 +1,7 @@
 directory '/etc/shinken/hosts' do
-  owner     'shinken'
-  group     'shinken'
   recursive true
 end
 
-file '/etc/shinken/hosts/host-to-delete.cfg' do
-  owner  'shinken'
-  group  'shinken'
-end
+file '/etc/shinken/hosts/host-to-delete.cfg'
 
-file '/etc/shinken/hostgroups/hostgroup-to-delete.cfg' do
-  owner  'shinken'
-  group  'shinken'
-end
+file '/etc/shinken/hostgroups/hostgroup-to-delete.cfg'
