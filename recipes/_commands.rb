1ada60c95ad5b1adf1e0a47ddc414854d0bac151
Jeff Byrnes <jeff@evertrue.com>
2015-10-07 13:14:46 -0400

    Add headers to recipes

--- recipes/_commands.rb
+++ recipes/_commands.rb
@@ -1,3 +1,22 @@
+#
+# Cookbook Name:: shinken
+# Recipe:: _commands
+#
+# Copyright (C) 2014 EverTrue, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 node['shinken']['commands'].each do |cmd_name, cmd_conf|
   template "#{node['shinken']['conf_dir']}/commands/#{cmd_name}.cfg" do
     source 'generic-definition.cfg.erb'
