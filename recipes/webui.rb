a52b003a4b8521d1436fb2e423544b88696a9d58
Eric Herot <eric.github@herot.com>
2016-09-29 14:32:50 -0400

    Add copy-button patch to the webui code

--- recipes/webui.rb
+++ recipes/webui.rb
@@ -66,3 +66,16 @@ template '/etc/shinken/modules/webui2.cfg' do
   group  node['shinken']['group']
   notifies :restart, 'service[shinken]'
 end
+
+# Manually inserts Eric's custom-made copy to clipboard button
+# a la https://github.com/shinken-monitoring/mod-webui/pull/544
+%w(
+  views/eltdetail.tpl
+  htdocs/css/eltdetail.css
+  htdocs/js/eltdetail.js
+).each do |filename|
+  cookbook_file "/var/lib/shinken/modules/webui2/plugins/eltdetail/#{filename}" do
+    source "webui2/#{File.basename filename}"
+    owner  node['shinken']['user']
+  end
+end
