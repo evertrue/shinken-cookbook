01fbb0d81951ac4ce4c4436a76e754f66a976d7c
Eric Herot <eric.github@herot.com>
2016-11-01 16:51:26 -0400

    Version bump 2.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'Apache 2.0'
 description      'Installs/Configures shinken'
 long_description 'Installs/Configures shinken'
-version          '2.1.0'
+version          '2.1.1'
 
 depends 'ark'
 depends 'apt'
