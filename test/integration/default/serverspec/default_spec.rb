58b29dabcd0edbb145e8e5f4749c2d1909d498da
Eric Herot <eric.github@herot.com>
2017-01-05 16:03:31 -0500

    Test that something is listening on tcp/50000

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -130,7 +130,8 @@ describe 'Shinken Services' do
     7769,
     7771,
     7772,
-    7773
+    7773,
+    50000
   ].each do |p|
     context port(p) do
       # it { should be_listening.on('0.0.0.0').with('tcp') }
