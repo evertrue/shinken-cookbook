5c0b07a0cdaeaeaa3b851c1f4f00b3b188ccd49b
Eric Herot <eric.github@herot.com>
2017-01-05 16:22:27 -0500

    Attribute-ize livestatus configuration

--- attributes/broker.rb
+++ attributes/broker.rb
@@ -1,5 +1,11 @@
 default['shinken']['broker'] = {
   'port' => '7772',
+  'livestatus' => {
+    'port' => 50_000,
+    'modules' => [
+      'logstore-sqlite'
+    ]
+  },
   'modules' => [
     'webui2',
     'livestatus'
